166ba799636fdd3ceef1da84ab369711dea4761f
thunghia <82324283+thunghia@users.noreply.github.com>
2022-11-10 22:50:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,13 +10,13 @@ const CONFIG = {
     btnIntro: 'Next',
     title: 'Em vô tình đi ngang qua, gieo vào anh một hạt giống, ngỡ là cỏ dại...',
     desc: 'Đến khi ngoảnh lại đã thấy một rừng hoa.',
-    btnYes: 'Em đồng ý',
-    btnNo: 'No',
+    btnYes: 'Em đồng ý.',
+    btnNo: 'No.',
     question:'Thế giới 7 tỉ nhân loại này xác suất để hai người gặp nhau là 0.0000005 nhưng để 2 cá thể yêu nhau còn thấp hơn vài chục lần, em nghĩ sao về cuộc gặp gỡ này?',
     btnReply: 'Gửi',
     reply: 'Đây chắc chắn là mối nhân duyên trời định, tam sinh tam thế, romeo và juliet happy ending',
     mess: '...',
-    messDesc: 'Nếu thấy website chất lượng, liên hệ tôi',
-    btnAccept: 'Truy cập trang fb',
+    messDesc: 'Nếu thấy website chất lượng, liên hệ tôi.',
+    btnAccept: 'Truy cập trang fb.',
     messLink: 'https://www.facebook.com/andysupernumberone/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
